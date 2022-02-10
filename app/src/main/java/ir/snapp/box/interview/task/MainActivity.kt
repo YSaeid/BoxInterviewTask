6d4edcf5d80c9884b2f24f5f8bb7a1f9302359fe
Saeid Yazdani <saeidyazdany@gmail.com>
2022-02-10 14:28:41 +0330

    initialize ActivityMainBinding lazy lines (9 - 11) use mapView and set load style to MAPBOX_STREETS, line 15

--- app/src/main/java/ir/snapp/box/interview/task/MainActivity.kt
+++ app/src/main/java/ir/snapp/box/interview/task/MainActivity.kt
@@ -2,10 +2,16 @@ package ir.snapp.box.interview.task
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mapbox.maps.Style
+import ir.snapp.box.interview.task.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS)
     }
 }
